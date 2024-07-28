dbbdfcf28aee3eadd8270a21ff25515525ca468d
danielbui12 <huytung139@gmail.com>
2024-07-28 15:40:50 +0700

    bookadot-indexer

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 ## Bookadot EVM indexer
-
-### How to start
-
-```sh
-docker compose up -d
-yarn dev
-```
--- ponder.schema.ts
+++
@@ -1,8 +0,0 @@
-import { createSchema } from "@ponder/core";
-
-export default createSchema((p) => ({
-  Account: p.createTable({
-    id: p.hex(),
-    balance: p.bigint(),
-  }),
-}));
